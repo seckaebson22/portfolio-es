cc1730074526e608e737beb47cd5c58ea65a77a8
Ebrima Secka <seckaebson22@gmail.com>
2022-07-29 19:09:44 +0000

    Updates

---
+++ precache-manifest.1c0772b79a45acc60a0697cd8bf6979e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "56998fa9209f08237fc2",
+    "url": "/seckaebson22/portfolio-es/static/css/main.0b6cded5.chunk.css"
+  },
+  {
+    "revision": "56998fa9209f08237fc2",
+    "url": "/seckaebson22/portfolio-es/static/js/main.56998fa9.chunk.js"
+  },
+  {
+    "revision": "47e649446070bb12aa50",
+    "url": "/seckaebson22/portfolio-es/static/js/1.47e64944.chunk.js"
+  },
+  {
+    "revision": "37de11e29a7878320513",
+    "url": "/seckaebson22/portfolio-es/static/js/runtime~main.37de11e2.js"
+  },
+  {
+    "revision": "97b4b32cb297659809b8da37c31e74f9",
+    "url": "/seckaebson22/portfolio-es/static/media/logo.97b4b32c.jpg"
+  },
+  {
+    "revision": "9c617f6284024c07e6b7567a225668d7",
+    "url": "/seckaebson22/portfolio-es/static/media/github-npm.9c617f62.png"
+  },
+  {
+    "revision": "9312f1fb2de871f398b8f3e5307cc725",
+    "url": "/seckaebson22/portfolio-es/static/media/Resume.9312f1fb.pdf"
+  },
+  {
+    "revision": "794056b5719e1ad45388369d8d254046",
+    "url": "/seckaebson22/portfolio-es/index.html"
+  }
+];
