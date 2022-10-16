72375d964cdf0c52ace0fedb7e51209457184c2b
Ebrima Secka <64589275+seckaebson22@users.noreply.github.com>
2022-10-16 13:11:54 +0000

    Updates

---
+++ precache-manifest.fa40693aa4d6327a4f6c0e8a0058d409.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "70c5b2d5d47c2af545a3",
+    "url": "/portfolio-es/static/css/main.51f23348.chunk.css"
+  },
+  {
+    "revision": "70c5b2d5d47c2af545a3",
+    "url": "/portfolio-es/static/js/main.70c5b2d5.chunk.js"
+  },
+  {
+    "revision": "9290f084429cb1bda019",
+    "url": "/portfolio-es/static/js/1.9290f084.chunk.js"
+  },
+  {
+    "revision": "c44b222507f100ce4a9b",
+    "url": "/portfolio-es/static/js/runtime~main.c44b2225.js"
+  },
+  {
+    "revision": "97b4b32cb297659809b8da37c31e74f9",
+    "url": "/portfolio-es/static/media/logo.97b4b32c.jpg"
+  },
+  {
+    "revision": "378298a8f4bff2e5b0d54bc746c2840d",
+    "url": "/portfolio-es/static/media/award.378298a8.jpg"
+  },
+  {
+    "revision": "0db8a946cafb5f9840b805fff7a4fb83",
+    "url": "/portfolio-es/static/media/Education.0db8a946.jpg"
+  },
+  {
+    "revision": "33096ccaa3061fa0c00eeb2fc02f5821",
+    "url": "/portfolio-es/static/media/Resume.33096cca.pdf"
+  },
+  {
+    "revision": "db77e997749270bc1a35c1b55d08da5e",
+    "url": "/portfolio-es/static/media/cv-uk.db77e997.png"
+  },
+  {
+    "revision": "56b8576fb067587eae54c0f19d548ef3",
+    "url": "/portfolio-es/static/media/link-britannia.56b8576f.png"
+  },
+  {
+    "revision": "9cf9b42916773dfb2ce35cbbd205b95f",
+    "url": "/portfolio-es/static/media/fresh-and-funky.9cf9b429.jpg"
+  },
+  {
+    "revision": "63ae15cdf3740deefd2f207abb1336f8",
+    "url": "/portfolio-es/static/media/shady-tints.63ae15cd.png"
+  },
+  {
+    "revision": "b0f0805d366847f408727833ef6fbd44",
+    "url": "/portfolio-es/static/media/the-baae-company.b0f0805d.png"
+  },
+  {
+    "revision": "12f284f69cd210607cf46bd4e7da8785",
+    "url": "/portfolio-es/static/media/ebaaba-vendor-dashboard.12f284f6.png"
+  },
+  {
+    "revision": "6ee88a6cfe817cfee9301e46cc43bf7c",
+    "url": "/portfolio-es/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio-es/precache-manifest.884c59de86bf36937da891b20779b133.js"
+  "/portfolio-es/precache-manifest.fa40693aa4d6327a4f6c0e8a0058d409.js"
 );
 
 workbox.clientsClaim();
